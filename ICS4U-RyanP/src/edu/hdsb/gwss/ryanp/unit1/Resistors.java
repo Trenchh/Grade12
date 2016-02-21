af47e39466ba14b7731da3bf04d78e3936286b23
Swag God <Swag God@192.168.2.20>
2016-02-20 22:42:41 -0500

    - completed eratosthenes - started resistors

---
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit1/Resistors.java
@@ -0,0 +1,30 @@
+/*
+ * Name: Eratosthenes.java
+ * Date: February,20th 2016
+ * Version: v0.1
+ * Author: Ryan Protheroe
+ */
+package edu.hdsb.gwss.ryanp.unit1;
+
+/**
+ *
+ * @author Swag God
+ */
+public class Resistors {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String colours[] = {"black", "brown", "red", "orange", "yellow", "green",
+            "blue", "violet", "grey", "white"};
+        
+        
+        
+        
+        System.out.println("Please enter ");
+
+    }
+
+}
