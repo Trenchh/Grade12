e061aa31fb0a9ba806ea231ae8e310edf8f60b67
Swag God <Swag God@192.168.2.20>
2016-04-02 18:25:43 -0400

    - completed RecursiveFunctions

--- ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit2/culminating/RecursiveFunctions.java
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit2/culminating/RecursiveFunctions.java
@@ -17,7 +17,33 @@ public class RecursiveFunctions {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("The function returns: " + FunctionOne(5, 3));
+        System.out.println("The function returns: " + FunctionOne(2, 2));
+        System.out.println("The function returns: " + FunctionOne(1, 4));
+        System.out.println("The function returns: " + FunctionOne(-5, -2));
+        System.out.println("The function returns: " + FunctionTwo(1, 1));
+        System.out.println("The function returns: " + FunctionTwo(2, 1));
+        System.out.println("The function returns: " + FunctionTwo(5, 5));
     }
-    
+
+    public static int FunctionOne(int x, int y) {
+        if (x < y) {
+            return -FunctionOne(y, x);
+        } else if (x == y) {
+            return 0;
+        } else {
+            return 1 + FunctionOne((x - 1), y);
+        }
+    }
+
+    public static int FunctionTwo(int m, int n) {
+        if (m == 0) {
+            return n + 1;
+        } else if (m > 0 & n == 0) {
+            return FunctionTwo(m - 1, 1);
+        } else {
+            return FunctionTwo(m - 1, FunctionTwo(m, n - 1));
+        }
+    }
+
 }
