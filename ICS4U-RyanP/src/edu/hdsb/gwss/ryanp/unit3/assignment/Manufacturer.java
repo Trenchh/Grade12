b586d738b264dcde3cb225f54f61def63d40d983
1protheroery <1protheroery@gws33869.ihdsb.ca>
2016-04-13 13:38:28 -0400

    - started assignment

---
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit3/assignment/Manufacturer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.ryanp.unit3.assignment;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class Manufacturer {
+    
+    private String name;
+    private String location;
+    //private String location;
+    private ArrayList<String[][]> products = new ArrayList<String[][]>();
+    
+}
---
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit3/assignment/Toy.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.ryanp.unit3.assignment;
+
+/**
+ *
+ * @author 1protheroery
+ */
+public class Toy {
+
+    private String name;
+    private String manufacturer;
+    private String genre;
+    private String modelNumber;
+    private int price;
+
+}
