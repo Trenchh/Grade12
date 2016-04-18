c5d83e0f1caeba702ae4e52db3d935ba5fa8a706
Swag God <Swag God@192.168.2.20>
2016-04-17 20:45:02 -0400

    - working on assignment - lost

--- ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit3/assignment/Toy.java
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit3/assignment/Toy.java
@@ -12,12 +12,12 @@ package edu.hdsb.gwss.ryanp.unit3.assignment;
 public class Toy {
 
     //CLASS CONSTANT
-   // private static final String MANUFACTURER = Manufacturer.getName();
-
+    // private static final String MANUFACTURER = Manufacturer.getName();
     //CLASS VARIABLE
     private static int lastIDUsed = 0;
 
     private String name;
+    private String ModelNumber;
     private int storeID;
     private double price;
     private boolean sold;
@@ -42,6 +42,40 @@ public class Toy {
     public Toy(String name, int storeID, double price) {
         this(name, price);
         this.storeID = storeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.length() > 50) {
+            System.out.println("Invalid name");
+        } else {
+            this.name = name;
+
+        }
+    }
+
+    public String getModelNumber() {
+        return ModelNumber;
+    }
+
+    public void setModelNumber(String ModelNumber) {
+        //RANDOMIZER
+        this.ModelNumber = ModelNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            System.out.println("Invalid price");
+        } else {
+            this.price = price;
+        }
     }
 
 }
