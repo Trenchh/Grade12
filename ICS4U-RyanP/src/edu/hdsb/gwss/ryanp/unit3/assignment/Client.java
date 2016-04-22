76e4abb045d8c77faf276340e741f090d7eac843
Swag God <Swag God@192.168.2.20>
2016-04-22 14:22:00 -0400

    - Done assignment

--- ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit3/assignment/Client.java
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit3/assignment/Client.java
@@ -15,6 +15,7 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Manufacturer fisherPrice = new Manufacturer();
         //----------------------------------------------------------------------
         //TOY CLASS
         //----------------------------------------------------------------------
@@ -73,6 +74,46 @@ public class Client {
         assert (boat.getAgeRange() == 3);
         assert (boat.getAgeRangeName().equals("KID"));
 
+        // TEST #4: equals() same object
+        // PRE-CONDTION:
+        //      - none
+        // POST-CONDTION:
+        //      - object equals the same object
+        //      - returns true
+        System.out.println("-------------------");
+        System.out.println("Test #4 - equals() same object");
+        boat.setManufacturer(fisherPrice);
+        assert (boat.equals(boat));
+
+        // TEST #5: equals() different object
+        // PRE-CONDTION:
+        //      - none
+        // POST-CONDTION:
+        //      - object equals the same object
+        //      - returns true
+        System.out.println("-------------------");
+        System.out.println("Test #5 - equals() different object");
+        assert (!boat.equals(car));
+
+        // TEST #6: isValid()
+        // PRE-CONDTION:
+        //      - valid object
+        // POST-CONDTION:
+        //      - returns true
+        System.out.println("-------------------");
+        System.out.println("Test #6 - isValid()");
+        assert (boat.isValid() == true);
+
+        // TEST #7: isValid() not valid object
+        // PRE-CONDTION:
+        //      - invalid object
+        // POST-CONDTION:
+        //      - returns false
+        System.out.println("-------------------");
+        System.out.println("Test #7 - isValid() not valid object");
+        Toy lego = new Toy("lego", 7, 19.99);
+        assert (lego.isValid() == false);
+
         //----------------------------------------------------------------------
         //MANUFACTURER CLASS
         //----------------------------------------------------------------------
@@ -87,8 +128,8 @@ public class Client {
         System.out.println("-------------------");
         System.out.println("Test #1 - Empty Constructor");
         Manufacturer mattel = new Manufacturer();
-        assert (mattel.getFactoryID() == 1);
-        assert (mattel.getName().equals("MANUFACTURER #1"));
+        assert (mattel.getFactoryID() == 2);
+        assert (mattel.getName().equals("MANUFACTURER #2"));
         assert (mattel.isInBusiness() == true);
         assert (mattel.getLocation() == -1);
 
@@ -103,7 +144,7 @@ public class Client {
         System.out.println("-------------------");
         System.out.println("Test #2 - Constructor w/ Name");
         Manufacturer hasbro = new Manufacturer("Hasbro");
-        assert (hasbro.getFactoryID() == 2);
+        assert (hasbro.getFactoryID() == 3);
         assert (hasbro.getName().equals("HASBRO"));
         assert (hasbro.isInBusiness() == true);
         assert (hasbro.getLocation() == -1);
@@ -121,7 +162,7 @@ public class Client {
         System.out.println("-------------------");
         System.out.println("Test #3 - Constructor 'Valid Manufacturer'");
         Manufacturer tonka = new Manufacturer("Tonka", 3);
-        assert (tonka.getFactoryID() == 3);
+        assert (tonka.getFactoryID() == 4);
         assert (tonka.getName().equals("TONKA"));
         assert (tonka.isInBusiness() == true);
         assert (tonka.getLocation() == 3);
@@ -135,42 +176,50 @@ public class Client {
         System.out.println("Test #4 - isValid()");
         assert (tonka.isValid() == true);
 
-        // TEST #5: add()
+        // TEST #5: isValid() not valid object
+        // PRE-CONDTION:
+        //      - invalid object
+        // POST-CONDTION:
+        //      - returns false
+        System.out.println("-------------------");
+        System.out.println("Test #5 - isValid() not valid object");
+        Manufacturer littleTikes = new Manufacturer("Little Tikes", 13);
+        assert (littleTikes.isValid() == false);
+
+        // TEST #6: add()
         // PRE-CONDTION:
         //      - valid object
         // POST-CONDTION:
         //      - adds toy to list
         System.out.println("-------------------");
-        System.out.println("Test #5 - add() valid object");
-        boat.setManufacturer(mattel);
+        System.out.println("Test #6 - add() valid object");
         mattel.add(boat);
         assert (mattel.getProducts().size() == 1);
         assert (mattel.getProducts().contains(boat));
 
-        // TEST #5: add()
+        // TEST #7: add()
         // PRE-CONDTION:
         //      - invalid object
         // POST-CONDTION:
         //      - doesn't add toy to list
         System.out.println("-------------------");
-        System.out.println("Test #5 - add() invalid object");
-        Toy lego = new Toy("lego", 7, 19.99);
+        System.out.println("Test #7 - add() invalid object");
         lego.setManufacturer(mattel);
         mattel.add(lego);
         assert (mattel.getProducts().size() == 1);
 
-        // TEST #6: remove() valid object
+        // TEST #8: remove() valid object
         // PRE-CONDTION:
         //      - valid object
         // POST-CONDTION:
         //      - removes toy
         //      - empty array
         System.out.println("-------------------");
-        System.out.println("Test #6 - remove() valid object");
+        System.out.println("Test #8 - remove() valid object");
         mattel.remove(boat);
         assert (mattel.getProducts().isEmpty());
 
-        // TEST #7: remove() invald object
+        // TEST #9: remove() invald object
         // PRE-CONDTION:
         //      - re-add boat
         //      - try to remove invalid object
@@ -178,43 +227,40 @@ public class Client {
         //      - doesn't remove toy
         //      - array size is 1 and contains boat
         System.out.println("-------------------");
-        System.out.println("Test #7 - remove() invalid object");
+        System.out.println("Test #9 - remove() invalid object");
         mattel.add(boat);
         mattel.remove(lego);
         assert (mattel.getProducts().size() == 1);
         assert (mattel.getProducts().contains(boat));
 
-        // TEST #8: get() valid object
+        // TEST #10: get() valid object
         // PRE-CONDTION:
         //      - none
         // POST-CONDTION:
         //      - name of toy is returned as whole object
         System.out.println("-------------------");
-        System.out.println("Test #8 - get() valid object");
+        System.out.println("Test #10 - get() valid object");
         assert (mattel.get("boat").equals(boat));
 
-        // TEST #9: equals() same object
+        // TEST #11: equals() same object
         // PRE-CONDTION:
         //      - none
         // POST-CONDTION:
         //      - object equals the same object
         //      - returns true
         System.out.println("-------------------");
-        System.out.println("Test #9 - equals() same object");
+        System.out.println("Test #11 - equals() same object");
         assert (mattel.equals(mattel));
-        
-        // TEST #10: equals() different object
+
+        // TEST #12: equals() different object
         // PRE-CONDTION:
         //      - none
         // POST-CONDTION:
         //      - object equals the same object
         //      - returns true
         System.out.println("-------------------");
-        System.out.println("Test #10 - equals() different object");
+        System.out.println("Test #12 - equals() different object");
         assert (!mattel.equals(tonka));
-        
-        
-        
 
     }
 }
