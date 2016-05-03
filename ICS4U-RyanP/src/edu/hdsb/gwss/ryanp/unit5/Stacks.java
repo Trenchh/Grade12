2b368db04afb9c1b8a3cd3a599629f49cb0900dc
Swag God <Swag God@192.168.2.20>
2016-05-02 22:51:04 -0400

    - began stacks

---
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit5/Stacks.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.ryanp.unit5;
+
+/**
+ *
+ * @author Swag God
+ */
+public class Stacks implements StackInterface {
+
+    private int[] stack;
+    private int top;
+
+    public Stacks() {
+        
+    }
+
+    public Stacks(int length) {
+        
+    }
+
+    public int top() {
+        
+    }
+
+    public int pop() {
+
+    }
+
+    public void push(int value) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public int capacity() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public boolean isFull() {
+
+    }
+
+    public void makeEmpty() {
+
+    }
+
+}
