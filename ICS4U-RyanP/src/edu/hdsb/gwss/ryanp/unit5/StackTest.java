8e77712b341dd3cf63919ceef9da13ea8afd0b01
Swag God <Swag God@192.168.2.20>
2016-05-03 23:05:04 -0400

    - completed stack and testing

--- ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit5/StackTest.java
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit5/StackTest.java
@@ -15,7 +15,6 @@ public class StackTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         Stack stack = new Stack();
 
         // EMPTY
@@ -29,23 +28,60 @@ public class StackTest {
         // ADD ONE VALUE
         stack.push(1);
         assert (stack.top() == 1);
-        //assert (stack.pop() == -1);
         assert (stack.size() == 1);
         assert (stack.capacity() == Stack.DEFAULT_SIZE);
         assert (stack.isEmpty() == false);
         assert (stack.isFull() == false);
 
-//        System.out.println( stack.toString() );
-//        stack.push(25);
-//        System.out.println( stack.toString() );
-//        System.out.println(stack.top());
-//        stack.push(5);
-//        System.out.println( stack.toString() );
-//        stack.push(13);
-//        System.out.println( stack.toString() );
-//        System.out.println(stack.pop());
-//        System.out.println( stack.toString() );
-//        System.out.println(stack.top());
-//        System.out.println( stack.toString() );
+        // POP ONE VALUE
+        stack.pop();
+        assert (stack.top() == -1);
+        assert (stack.size() == 0);
+        assert (stack.capacity() == Stack.DEFAULT_SIZE);
+        assert (stack.isEmpty() == true);
+        assert (stack.isFull() == false);
+
+        // FILL STACK
+        for (int i = 0; i < stack.capacity(); i++) {
+            stack.push(i);
+        }
+        assert (stack.top() == 24);
+        assert (stack.size() == 25);
+        assert (stack.capacity() == Stack.DEFAULT_SIZE);
+        assert (stack.isEmpty() == false);
+        assert (stack.isFull() == true);
+
+        // PUSH WHEN FULL
+        stack.push(25);
+        assert (stack.top() == 24);
+        assert (stack.size() == 25);
+        assert (stack.capacity() == Stack.DEFAULT_SIZE);
+        assert (stack.isEmpty() == false);
+        assert (stack.isFull() == true);
+
+        // MAKE EMPTY
+        stack.makeEmpty();
+        assert (stack.top() == -1);
+        assert (stack.size() == 0);
+        assert (stack.capacity() == Stack.DEFAULT_SIZE);
+        assert (stack.isEmpty() == true);
+        assert (stack.isFull() == false);
+
+        // POP WHEN EMPTY
+        stack.pop();
+        assert (stack.top() == -1);
+        assert (stack.size() == 0);
+        assert (stack.capacity() == Stack.DEFAULT_SIZE);
+        assert (stack.isEmpty() == true);
+        assert (stack.isFull() == false);
+
+        Stack stack2 = new Stack(15);
+
+        // SETS CAPACITY TO 25
+        assert (stack2.top() == -1);
+        assert (stack2.size() == 0);
+        assert (stack2.capacity() == 15);
+        assert (stack2.isEmpty() == true);
+        assert (stack2.isFull() == false);
     }
 }
