811c0f123112a2247c2317e7b6771b7cb22a9200
Swag God <Swag God@192.168.2.20>
2016-05-10 22:34:23 -0400

    - working on linked list - addAtEnd done - remove done

--- ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit5/dataStructures/LinkedList/LinkedList.java
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit5/dataStructures/LinkedList/LinkedList.java
@@ -10,13 +10,13 @@ package edu.hdsb.gwss.ryanp.unit5.dataStructures.LinkedList;
  * @author 1protheroery
  */
 public class LinkedList implements LinkListInterface {
-
+    
     private Node head;
     private Node tail;
-
+    
     @Override
     public int size() {
-        if (this.isEmpty()) {
+        if (!this.isEmpty()) {
             int size = 0;
             Node n = this.head;
             while (n.getNext() != null) {
@@ -27,54 +27,65 @@ public class LinkedList implements LinkListInterface {
         }
         return 0;
     }
-
+    
     @Override
     public void makeEmpty() {
         Node n = this.head;
         Node tmp = n;
         while (n.getNext() != null) {
-            // FIX, IT WILL ERASE BEFORE GETTING NEXT tmp = n.getNext();
             tmp = n.getNext();
             n.setNext(null);
             n = tmp;
-
         }
     }
-
+    
     @Override
     public boolean isEmpty() {
-        return this.head.getData() == null;
+        return this.head == null;
     }
-
+    
     @Override
     public void addAtFront(String str) {
         Node front = new Node(str);
-        front.setNext(head);
+        front.setNext(this.head);
         this.head = front;
         if (this.isEmpty()) {
             this.tail = front;
         }
-
     }
-
+    
     @Override
     public void addAtEnd(String str) {
-
+        Node end = new Node(str);
+        end.setNext(this.tail);
+        this.tail = end;
+        if (this.isEmpty()) {
+            this.head = end;
+        }
     }
-
+    
     @Override
     public void remove(String str) {
-
+        Node n = this.head;
+        while (n.getNext() != null) {
+            if (n.getNext().getData() == str) {
+                n.getNext().setNext(null);
+                n.setNext(n.getNext().getNext());
+            } else {
+                n = n.getNext();
+            }
+        }
+        
     }
-
+    
     @Override
     public String removeFromEnd() {
-
+        
     }
-
+    
     @Override
     public String removeFromFront() {
-
+        
     }
-
+    
 }
