613de2bbcbdf05e3966c8daba99ab4020eff2dc8
Swag God <Swag God@192.168.2.20>
2016-05-24 23:13:24 -0400

    - worked on phone

--- ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit7/randomAccessFiles/Phone.java
+++ ICS4U-RyanP/src/edu/hdsb/gwss/ryanp/unit7/randomAccessFiles/Phone.java
@@ -16,11 +16,8 @@ public class Phone {
     NumberFormat money = NumberFormat.getCurrencyInstance();
 
     //CLASS CONSTANTS
-    private static final String BELL = "BELL";
-    private static final String ROGERS = "ROGERS";
-    private static final String TELUS = "TELUS";
-    private static final String KOODO = "KOODO";
-    private static final String WIND = "WIND";
+    private static final String[] CARRIERS = {"BELL", "ROGERS", "TELUS", "KOODO", "WIND", "VIRGIN"};
+    private static final int NOT_SET = -1;
 
     //CLASS VARIABLE
     private static int lastIDUsed = 0;
@@ -29,7 +26,87 @@ public class Phone {
     private int storage;
     private String carrier;
     private double price;
+    private int phoneID;
     private int number;
     private String OS;
 
+    public Phone() {
+        this.setPhoneID();
+        this.setName("Phone #" + this.phoneID);
+        this.setStorage(NOT_SET);
+        this.setCarrier(null);
+
+    }
+
+    public void setNumber(int number) {
+        if (Integer.toString(number).length() == 10) {
+            this.number = number;
+        } else {
+            this.number = NOT_SET;
+            System.out.println("INVALID NUMBER");
+        }
+    }
+
+    public int getNumber() {
+        if (this.number == NOT_SET) {
+            System.out.println("SET NUMBER");
+        } else {
+            System.out.println("THE NUMBER FOR " + this.name + " IS " + this.number);
+        }
+        return number;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = null;
+        for (int i = 0; i < CARRIERS.length; i++) {
+            if (carrier.toUpperCase().equals(CARRIERS[i])) {
+                this.carrier = carrier;
+            }
+        }
+    }
+
+    public String getCarrier() {
+        if (this.carrier == null) {
+            System.out.println("SET CARRIER");
+        }
+        return carrier;
+    }
+
+    public void setStorage(int amount) {
+        if (amount <= 0) {
+            this.storage = NOT_SET;
+            System.out.println("INVALID AMOUNT");
+
+        } else {
+            this.storage = amount;
+        }
+    }
+
+    public int getStorage() {
+        if (storage == NOT_SET) {
+            System.out.println("SET AMOUNT OF STORAGE");
+        }
+        return storage;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.length() > 20) {
+            System.out.println("INVALID NAME");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private int setPhoneID() {
+        this.phoneID = ++lastIDUsed;
+        return phoneID;
+    }
+
+    public int getPhoneID() {
+        return phoneID;
+    }
 }
